972408ea187646eb5ce7b127115c5b84adec8ad1
HybridDog <adresse_test@t-online.de>
2013-06-23 15:32:04 +0200

    load my version

--- init.lua
+++ init.lua
@@ -1,46 +1,141 @@
--- Nuke Mod 1.5 by sfan5
--- Licensed under GPLv2
+local nuke_preserve_items = true
+local nuke_drop_items = false --this will only cause lags
+
+local function describe_chest()
+	if math.random(5) == 1 then return "You nuked. I HAVE NOT!" end
+	if math.random(10) == 1 then return "Hehe, I'm the result of your explosion hee!" end
+	if math.random(20) == 1 then return "Look into me, I'm fat!" end
+	if math.random(30) == 1 then return "Please don't rob me. Else you are as evil as the other persons who took my inventoried stuff." end
+	if math.random(300) == 1 then return "I'll follow you until I ate you. Like I did with the other objects here..." end
+	return "Feel free to take the nuked items out of me!"
+end
+
+
+
+local function set_chest(p) --add a chest if the previous one is full
+	local pos = p
+	while minetest.env:get_node({x=pos.x, y=pos.y-1, z=pos.z}).name == "air" do
+		pos.y=pos.y-1
+	end
+	minetest.env:add_node(pos, {name="default:chest"})
+	local meta = minetest.get_meta(pos)
+	meta:set_string("formspec",default.chest_formspec)
+	meta:set_string("infotext", describe_chest())
+	local inve = meta:get_inventory()
+	inve:set_size("main", 8*4)
+	nuke_chestpos = pos
+end
+
+local function destroy_node(pos)
+	if nuke_preserve_items then
+		local drops = minetest.get_node_drops(minetest.env:get_node(pos).name)
+		minetest.env:remove_node(pos)
+		if nuke_drop_items then
+			for _, item in ipairs(drops) do
+				if item ~= "default:cobble" then
+					minetest.env:add_item(pos, item)
+				end
+			end
+		elseif nuke_puncher ~= nil then
+			local inv = nuke_puncher:get_inventory()
+			if inv then
+				for _, item in ipairs(drops) do
+					if inv:room_for_item("main", item) then
+						inv:add_item("main", item)
+					else
+						if nuke_chestpos == nil then
+							set_chest(pos)
+						end
+						local chestinv = minetest.get_meta(nuke_chestpos):get_inventory()
+						if not chestinv:room_for_item("main", item) then
+							set_chest(pos)
+						end
+						chestinv:add_item("main", item)
+					end
+				end
+			end
+		end
+	else
+		minetest.env:remove_node(pos)
+	end
+end
 
 function spawn_tnt(pos, entname)
-    minetest.sound_play("nuke_ignite", {pos = pos,gain = 1.0,max_hear_distance = 8,})
-    return minetest.env:add_entity(pos, entname)
+	minetest.sound_play("nuke_ignite", {pos = pos,gain = 1.0,max_hear_distance = 8,})
+	return minetest.env:add_entity(pos, entname)
 end
 
 function activate_if_tnt(nname, np, tnt_np, tntr)
-    if nname == "nuke:iron_tnt" or nname == "nuke:mese_tnt" or nname == "nuke:hardcore_iron_tnt" or nname == "nuke:hardcore_mese_tnt" then
-        local e = spawn_tnt(np, nname)
-        e:setvelocity({x=(np.x - tnt_np.x)*3+(tntr / 4), y=(np.y - tnt_np.y)*3+(tntr / 3), z=(np.z - tnt_np.z)*3+(tntr / 4)})
-    end
+	if nname == "experimental:tnt" or nname == "nuke:iron_tnt" or nname == "nuke:mese_tnt" or nname == "nuke:hardcore_iron_tnt" or nname == "nuke:hardcore_mese_tnt" then
+		local e = spawn_tnt(np, nname)
+		e:setvelocity({x=(np.x - tnt_np.x)*3+(tntr / 4), y=(np.y - tnt_np.y)*3+(tntr / 3), z=(np.z - tnt_np.z)*3+(tntr / 4)})
+	end
 end
 
 function do_tnt_physics(tnt_np,tntr)
-    local objs = minetest.env:get_objects_inside_radius(tnt_np, tntr)
-    for k, obj in pairs(objs) do
-        local oname = obj:get_entity_name()
-        local v = obj:getvelocity()
-        local p = obj:getpos()
-        if oname == "nuke:iron_tnt" or oname == "nuke:mese_tnt" or oname == "nuke:hardcore_iron_tnt" or oname == "nuke:hardcore_mese_tnt" then
-            obj:setvelocity({x=(p.x - tnt_np.x) + (tntr / 2) + v.x, y=(p.y - tnt_np.y) + tntr + v.y, z=(p.z - tnt_np.z) + (tntr / 2) + v.z})
-        else
-            if v ~= nil then
-                obj:setvelocity({x=(p.x - tnt_np.x) + (tntr / 4) + v.x, y=(p.y - tnt_np.y) + (tntr / 2) + v.y, z=(p.z - tnt_np.z) + (tntr / 4) + v.z})
-            else
-                if obj:get_player_name() ~= nil then
-                    obj:set_hp(obj:get_hp() - 1)
-                end
-            end
-        end
-    end
+	local objs = minetest.env:get_objects_inside_radius(tnt_np, tntr)
+	for k, obj in pairs(objs) do
+		local oname = obj:get_entity_name()
+		local v = obj:getvelocity()
+		local p = obj:getpos()
+		if oname == "experimental:tnt" or oname == "nuke:iron_tnt" or oname == "nuke:mese_tnt" or oname == "nuke:hardcore_iron_tnt" or oname == "nuke:hardcore_mese_tnt" then
+			obj:setvelocity({x=(p.x - tnt_np.x) + (tntr / 2) + v.x, y=(p.y - tnt_np.y) + tntr + v.y, z=(p.z - tnt_np.z) + (tntr / 2) + v.z})
+		else
+			if v ~= nil then
+				obj:setvelocity({x=(p.x - tnt_np.x) + (tntr / 4) + v.x, y=(p.y - tnt_np.y) + (tntr / 2) + v.y, z=(p.z - tnt_np.z) + (tntr / 4) + v.z})
+			else
+				if obj:get_player_name() ~= nil then
+					obj:set_hp(obj:get_hp() - 1)
+				end
+			end
+		end
+	end
+end
+
+local function explode(pos, range)
+	local radius = range^2 + range
+	for x=-range,range do
+		for y=-range,range do
+			for z=-range,range do
+				local r = x^2+y^2+z^2 
+				if r <= radius then
+					local np={x=pos.x+x, y=pos.y+y, z=pos.z+z}
+					local n = minetest.env:get_node(np)
+					if n.name ~= "air"
+					and n.name ~= "default:chest" then
+	--				and math.random(1,2^rad) < range*8 then
+						if math.floor(math.sqrt(r) +0.5) > range-1 then
+							if math.random(1,5) >= 2 then
+								destroy_node(np)
+							elseif math.random(1,50) == 1 then
+								minetest.sound_play("default_glass_footstep", {pos = np, gain = 0.5, max_hear_distance = 4})
+							end
+						else
+							destroy_node(np)
+						end
+					--[[elseif n.name == "default:chest" then
+						local p = pos
+						while minetest.env:get_node({x=p.x, y=p.y-1, z=p.z}).name == "air" do
+							p.y=p.y-1
+						end
+						minetest.env:add_node(p, {name="default:chest"})
+						minetest.env:get_meta(minetest.env:get_meta(pos))]]
+					end
+					activate_if_tnt(n.name, np, pos, range)
+				end
+			end
+		end
+	end
 end
 
 -- Iron TNT
 
 minetest.register_craft({
-	output = 'nuke:iron_tnt 4',
+	output = 'node "nuke:iron_tnt" 4',
 	recipe = {
-		{'','default:wood 1',''},
-		{'default:steel_ingot 1','default:coal_lump 1','default:steel_ingot 1'},
-		{'','default:wood 1',''}
+		{'','node "default:wood" 1',''},
+		{'craft "default:steel_ingot" 1','craft "default:coal_lump" 1','craft "default:steel_ingot" 1'},
+		{'','node "default:wood" 1',''}
 	}
 })
 minetest.register_node("nuke:iron_tnt", {
@@ -56,11 +151,12 @@ minetest.register_node("nuke:iron_tnt", {
 	description = "Iron TNT",
 })
 
-minetest.register_on_punchnode(function(p, node)
+minetest.register_on_punchnode(function(p, node, puncher)
 	if node.name == "nuke:iron_tnt" then
 		minetest.env:remove_node(p)
 		spawn_tnt(p, "nuke:iron_tnt")
 		nodeupdate(p)
+		nuke_puncher = puncher
 	end
 end)
 
@@ -91,13 +187,13 @@ end
 function IRON_TNT:on_step(dtime)
 	self.timer = self.timer + dtime
 	self.blinktimer = self.blinktimer + dtime
-    if self.timer>5 then
-        self.blinktimer = self.blinktimer + dtime
-        if self.timer>8 then
-            self.blinktimer = self.blinktimer + dtime
-            self.blinktimer = self.blinktimer + dtime
-        end
-    end
+	if self.timer>5 then
+		self.blinktimer = self.blinktimer + dtime
+		if self.timer>8 then
+			self.blinktimer = self.blinktimer + dtime
+			self.blinktimer = self.blinktimer + dtime
+		end
+	end
 	if self.blinktimer > 0.5 then
 		self.blinktimer = self.blinktimer - 0.5
 		if self.blinkstatus then
@@ -109,30 +205,17 @@ function IRON_TNT:on_step(dtime)
 	end
 	if self.timer > 10 then
 		local pos = self.object:getpos()
-        pos.x = math.floor(pos.x+0.5)
-        pos.y = math.floor(pos.y+0.5)
-        pos.z = math.floor(pos.z+0.5)
-        do_tnt_physics(pos, IRON_TNT_RANGE)
-        minetest.sound_play("nuke_explode", {pos = pos,gain = 1.0,max_hear_distance = 16,})
-        if minetest.env:get_node(pos).name == "default:water_source" or minetest.env:get_node(pos).name == "default:water_flowing" then
-            -- Cancel the Explosion
-            self.object:remove()
-            return
-        end
-        for x=-IRON_TNT_RANGE,IRON_TNT_RANGE do
-        for y=-IRON_TNT_RANGE,IRON_TNT_RANGE do
-        for z=-IRON_TNT_RANGE,IRON_TNT_RANGE do
-            if x*x+y*y+z*z <= IRON_TNT_RANGE * IRON_TNT_RANGE + IRON_TNT_RANGE then
-                local np={x=pos.x+x,y=pos.y+y,z=pos.z+z}
-                local n = minetest.env:get_node(np)
-                if n.name ~= "air" then
-                    minetest.env:remove_node(np)
-                end
-                activate_if_tnt(n.name, np, pos, IRON_TNT_RANGE)
-            end
-        end
-        end
-        end
+		pos.x = math.floor(pos.x+0.5)
+		pos.y = math.floor(pos.y+0.5)
+		pos.z = math.floor(pos.z+0.5)
+		do_tnt_physics(pos, IRON_TNT_RANGE)
+		minetest.sound_play("nuke_explode", {pos = pos,gain = 1.0,max_hear_distance = 16,})
+		if minetest.env:get_node(pos).name == "default:water_source" or minetest.env:get_node(pos).name == "default:water_flowing" then
+			-- Cancel the Explosion
+			self.object:remove()
+			return
+		end
+		explode(pos, IRON_TNT_RANGE)
 		self.object:remove()
 	end
 end
@@ -150,11 +233,11 @@ minetest.register_entity("nuke:iron_tnt", IRON_TNT)
 -- Mese TNT
 
 minetest.register_craft({
-	output = 'nuke:mese_tnt 4',
+	output = 'node "nuke:mese_tnt" 4',
 	recipe = {
-		{'','default:wood 1',''},
-		{'default:mese_crystal 1','default:coal_lump 1','default:mese_crystal 1'},
-		{'','default:wood 1',''}
+		{'','node "default:wood" 1',''},
+		{'node "default:mese" 1','craft "default:coal_lump" 1','node "default:mese" 1'},
+		{'','node "default:wood" 1',''}
 	}
 })
 minetest.register_node("nuke:mese_tnt", {
@@ -170,11 +253,12 @@ minetest.register_node("nuke:mese_tnt", {
 	description = "Mese TNT",
 })
 
-minetest.register_on_punchnode(function(p, node)
+minetest.register_on_punchnode(function(p, node, puncher)
 	if node.name == "nuke:mese_tnt" then
 		minetest.env:remove_node(p)
 		spawn_tnt(p, "nuke:mese_tnt")
 		nodeupdate(p)
+		nuke_puncher = puncher
 	end
 end)
 
@@ -205,13 +289,13 @@ end
 function MESE_TNT:on_step(dtime)
 	self.timer = self.timer + dtime
 	self.blinktimer = self.blinktimer + dtime
-    if self.timer>5 then
-        self.blinktimer = self.blinktimer + dtime
-        if self.timer>8 then
-            self.blinktimer = self.blinktimer + dtime
-            self.blinktimer = self.blinktimer + dtime
-        end
-    end
+	if self.timer>5 then
+		self.blinktimer = self.blinktimer + dtime
+		if self.timer>8 then
+			self.blinktimer = self.blinktimer + dtime
+			self.blinktimer = self.blinktimer + dtime
+		end
+	end
 	if self.blinktimer > 0.5 then
 		self.blinktimer = self.blinktimer - 0.5
 		if self.blinkstatus then
@@ -223,30 +307,17 @@ function MESE_TNT:on_step(dtime)
 	end
 	if self.timer > 10 then
 		local pos = self.object:getpos()
-        pos.x = math.floor(pos.x+0.5)
-        pos.y = math.floor(pos.y+0.5)
-        pos.z = math.floor(pos.z+0.5)
-        do_tnt_physics(pos, MESE_TNT_RANGE)
-        minetest.sound_play("nuke_explode", {pos = pos,gain = 1.0,max_hear_distance = 16,})
-        if minetest.env:get_node(pos).name == "default:water_source" or minetest.env:get_node(pos).name == "default:water_flowing" then
-            -- Cancel the Explosion
-            self.object:remove()
-            return
-        end
-        for x=-MESE_TNT_RANGE,MESE_TNT_RANGE do
-        for y=-MESE_TNT_RANGE,MESE_TNT_RANGE do
-        for z=-MESE_TNT_RANGE,MESE_TNT_RANGE do
-            if x*x+y*y+z*z <= MESE_TNT_RANGE * MESE_TNT_RANGE + MESE_TNT_RANGE then
-                local np={x=pos.x+x,y=pos.y+y,z=pos.z+z}
-                local n = minetest.env:get_node(np)
-                if n.name ~= "air" then
-                    minetest.env:remove_node(np)
-                end
-                activate_if_tnt(n.name, np, pos, MESE_TNT_RANGE)
-            end
-        end
-        end
-        end
+		pos.x = math.floor(pos.x+0.5)
+		pos.y = math.floor(pos.y+0.5)
+		pos.z = math.floor(pos.z+0.5)
+		do_tnt_physics(pos, MESE_TNT_RANGE)
+		minetest.sound_play("nuke_explode", {pos = pos,gain = 1.0,max_hear_distance = 16,})
+		if minetest.env:get_node(pos).name == "default:water_source" or minetest.env:get_node(pos).name == "default:water_flowing" then
+			-- Cancel the Explosion
+			self.object:remove()
+			return
+		end
+		explode(pos, MESE_TNT_RANGE)
 		self.object:remove()
 	end
 end
@@ -264,11 +335,11 @@ minetest.register_entity("nuke:mese_tnt", MESE_TNT)
 -- Hardcore Iron TNT
 
 minetest.register_craft({
-	output = 'nuke:hardcore_iron_tnt 1',
+	output = 'node "nuke:hardcore_iron_tnt" 1',
 	recipe = {
-		{'','default:coal_lump 1',''},
-		{'default:coal_lump 1','nuke:iron_tnt 1','default:coal_lump 1'},
-		{'','default:coal_lump 1',''}
+		{'','craft "default:coal_lump" 1',''},
+		{'craft "default:coal_lump" 1','node "nuke:iron_tnt" 1','craft "default:coal_lump" 1'},
+		{'','craft "default:coal_lump" 1',''}
 	}
 })
 minetest.register_node("nuke:hardcore_iron_tnt", {
@@ -292,7 +363,7 @@ minetest.register_on_punchnode(function(p, node)
 	end
 end)
 
-local HARDCORE_IRON_TNT_RANGE = 4
+local HARDCORE_IRON_TNT_RANGE = 6
 local HARDCORE_IRON_TNT = {
 	-- Static definition
 	physical = true, -- Collides with things
@@ -319,13 +390,13 @@ end
 function HARDCORE_IRON_TNT:on_step(dtime)
 	self.timer = self.timer + dtime
 	self.blinktimer = self.blinktimer + dtime
-    if self.timer>5 then
-        self.blinktimer = self.blinktimer + dtime
-        if self.timer>8 then
-            self.blinktimer = self.blinktimer + dtime
-            self.blinktimer = self.blinktimer + dtime
-        end
-    end
+	if self.timer>5 then
+		self.blinktimer = self.blinktimer + dtime
+		if self.timer>8 then
+			self.blinktimer = self.blinktimer + dtime
+			self.blinktimer = self.blinktimer + dtime
+		end
+	end
 	if self.blinktimer > 0.5 then
 		self.blinktimer = self.blinktimer - 0.5
 		if self.blinkstatus then
@@ -337,18 +408,18 @@ function HARDCORE_IRON_TNT:on_step(dtime)
 	end
 	if self.timer > 10 then
 		local pos = self.object:getpos()
-        pos.x = math.floor(pos.x+0.5)
-        pos.y = math.floor(pos.y+0.5)
-        pos.z = math.floor(pos.z+0.5)
-        minetest.sound_play("nuke_explode", {pos = pos,gain = 1.0,max_hear_distance = 16,})
-        for x=-HARDCORE_IRON_TNT_RANGE,HARDCORE_IRON_TNT_RANGE do
-        for z=-HARDCORE_IRON_TNT_RANGE,HARDCORE_IRON_TNT_RANGE do
-            if x*x+z*z <= HARDCORE_IRON_TNT_RANGE * HARDCORE_IRON_TNT_RANGE + HARDCORE_IRON_TNT_RANGE then
-                local np={x=pos.x+x,y=pos.y,z=pos.z+z}
-                minetest.env:add_entity(np, "nuke:iron_tnt")
-            end
-        end
-        end
+		pos.x = math.floor(pos.x+0.5)
+		pos.y = math.floor(pos.y+0.5)
+		pos.z = math.floor(pos.z+0.5)
+		minetest.sound_play("nuke_explode", {pos = pos,gain = 1.0,max_hear_distance = 16,})
+		for x=-HARDCORE_IRON_TNT_RANGE,HARDCORE_IRON_TNT_RANGE do
+		for z=-HARDCORE_IRON_TNT_RANGE,HARDCORE_IRON_TNT_RANGE do
+			if x*x+z*z <= HARDCORE_IRON_TNT_RANGE * HARDCORE_IRON_TNT_RANGE + HARDCORE_IRON_TNT_RANGE then
+				local np={x=pos.x+x,y=pos.y,z=pos.z+z}
+				minetest.env:add_entity(np, "nuke:iron_tnt")
+			end
+		end
+		end
 		self.object:remove()
 	end
 end
@@ -357,7 +428,8 @@ function HARDCORE_IRON_TNT:on_punch(hitter)
 	self.health = self.health - 1
 	if self.health <= 0 then
 		self.object:remove()
-		hitter:get_inventory():add_item("main", "nuke:hardcore_iron_tnt 1")
+		hitter:add_to_inventory("node nuke:hardcore_iron_tnt 1")
+		hitter:set_hp(hitter:get_hp() - 1)
 	end
 end
 
@@ -366,11 +438,11 @@ minetest.register_entity("nuke:hardcore_iron_tnt", HARDCORE_IRON_TNT)
 -- Hardcore Mese TNT
 
 minetest.register_craft({
-	output = 'nuke:hardcore_mese_tnt 1',
+	output = 'node "nuke:hardcore_mese_tnt" 1',
 	recipe = {
-		{'','default:coal_lump 1',''},
-		{'default:coal_lump 1','nuke:mese_tnt 1','default:coal_lump 1'},
-		{'','default:coal_lump 1',''}
+		{'','craft "default:coal_lump" 1',''},
+		{'craft "default:coal_lump" 1','node "nuke:mese_tnt" 1','craft "default:coal_lump" 1'},
+		{'','craft "default:coal_lump" 1',''}
 	}
 })
 minetest.register_node("nuke:hardcore_mese_tnt", {
@@ -394,7 +466,7 @@ minetest.register_on_punchnode(function(p, node)
 	end
 end)
 
-local HARDCORE_MESE_TNT_RANGE = 4
+local HARDCORE_MESE_TNT_RANGE = 6
 local HARDCORE_MESE_TNT = {
 	-- Static definition
 	physical = true, -- Collides with things
@@ -421,13 +493,13 @@ end
 function HARDCORE_MESE_TNT:on_step(dtime)
 	self.timer = self.timer + dtime
 	self.blinktimer = self.blinktimer + dtime
-    if self.timer>5 then
-        self.blinktimer = self.blinktimer + dtime
-        if self.timer>8 then
-            self.blinktimer = self.blinktimer + dtime
-            self.blinktimer = self.blinktimer + dtime
-        end
-    end
+	if self.timer>5 then
+		self.blinktimer = self.blinktimer + dtime
+		if self.timer>8 then
+			self.blinktimer = self.blinktimer + dtime
+			self.blinktimer = self.blinktimer + dtime
+		end
+	end
 	if self.blinktimer > 0.5 then
 		self.blinktimer = self.blinktimer - 0.5
 		if self.blinkstatus then
@@ -439,18 +511,18 @@ function HARDCORE_MESE_TNT:on_step(dtime)
 	end
 	if self.timer > 10 then
 		local pos = self.object:getpos()
-        pos.x = math.floor(pos.x+0.5)
-        pos.y = math.floor(pos.y+0.5)
-        pos.z = math.floor(pos.z+0.5)
-        minetest.sound_play("nuke_explode", {pos = pos,gain = 1.0,max_hear_distance = 16,})
-        for x=-HARDCORE_MESE_TNT_RANGE,HARDCORE_MESE_TNT_RANGE do
-        for z=-HARDCORE_MESE_TNT_RANGE,HARDCORE_MESE_TNT_RANGE do
-            if x*x+z*z <= HARDCORE_MESE_TNT_RANGE * HARDCORE_MESE_TNT_RANGE + HARDCORE_MESE_TNT_RANGE then
-                local np={x=pos.x+x,y=pos.y,z=pos.z+z}
-                minetest.env:add_entity(np, "nuke:mese_tnt")
-            end
-        end
-        end
+		pos.x = math.floor(pos.x+0.5)
+		pos.y = math.floor(pos.y+0.5)
+		pos.z = math.floor(pos.z+0.5)
+		minetest.sound_play("nuke_explode", {pos = pos,gain = 1.0,max_hear_distance = 16,})
+		for x=-HARDCORE_MESE_TNT_RANGE,HARDCORE_MESE_TNT_RANGE do
+		for z=-HARDCORE_MESE_TNT_RANGE,HARDCORE_MESE_TNT_RANGE do
+			if x*x+z*z <= HARDCORE_MESE_TNT_RANGE * HARDCORE_MESE_TNT_RANGE + HARDCORE_MESE_TNT_RANGE then
+				local np={x=pos.x+x,y=pos.y,z=pos.z+z}
+				minetest.env:add_entity(np, "nuke:mese_tnt")
+			end
+		end
+		end
 		self.object:remove()
 	end
 end
@@ -459,13 +531,9 @@ function HARDCORE_MESE_TNT:on_punch(hitter)
 	self.health = self.health - 1
 	if self.health <= 0 then
 		self.object:remove()
-		hitter:get_inventory():add_item("main", "nuke:hardcore_mese_tnt 1")
+		hitter:add_to_inventory("node nuke:hardcore_mese_tnt 1")
+		hitter:set_hp(hitter:get_hp() - 1)
 	end
 end
 
 minetest.register_entity("nuke:hardcore_mese_tnt", HARDCORE_MESE_TNT)
-
-minetest.add_to_creative_inventory("nuke:iron_tnt")
-minetest.add_to_creative_inventory("nuke:mese_tnt")
-minetest.add_to_creative_inventory("nuke:hardcore_iron_tnt")
-minetest.add_to_creative_inventory("nuke:hardcore_mese_tnt")
